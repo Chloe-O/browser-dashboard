d45e6b8460145ffc6e23804ca8fa44a9f11f6036
Your Name <chloeosbourne@yahoo.co.uk>
2022-04-26 12:59:43 +0100

    image credits displayed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+const userProfile = document.getElementById("user-profile")
+
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then( res => res.json())
     .then( data => {
         let bgImg = data.urls.full
-        console.log(bgImg)
+        console.log(data)
         document.body.style.backgroundImage = `url(${bgImg})`
+        userProfile.innerHTML = `<a target="_blank" href="${data.user.links.html}">${data.user.username}<i class="fa-solid fa-up-right-from-square"></i> </a>`
     })
