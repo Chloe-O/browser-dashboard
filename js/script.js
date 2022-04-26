caede5199f58cc0a3c8b34ad5f93c3cc0dd192a1
Your Name <chloeosbourne@yahoo.co.uk>
2022-04-26 16:18:03 +0100

    catch error, alt bg img added

--- js/script.js
+++ js/script.js
@@ -8,3 +8,7 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(${bgImg})`
         authorProfile.innerHTML = `<a class="link-text" target="_blank" href="${data.user.links.html}">${data.user.username}  <i class="fa-solid fa-up-right-from-square"></i> </a>`
     })
+    .catch( (err) => {
+        console.log(err)
+        document.body.style.backgroundImage = `url(/media/alt-bg.jpg)`
+    })
--- readME.md
+++ readME.md
@@ -1,1 +1,2 @@
 https://css-tricks.com/perfect-full-page-background-image/#awesome-easy-progressive-css3-way - bg image 
+Photo by eberhard grossgasteiger: https://www.pexels.com/photo/down-angle-photography-of-red-clouds-and-blue-sky-844297/ - alt bg
