afa1ae825a16afb2c0f8f4b539f6cca1107972e0
Your Name <chloeosbourne@yahoo.co.uk>
2022-04-27 18:35:14 +0100

    weather icons, get weather data, styling

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//BG Image
+
 const authorProfile = document.getElementById("author-profile")
 
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
@@ -13,6 +15,8 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         authorProfile.innerHTML = `<a class="link-text" target="_blank" href="https://www.pexels.com/@eberhardgross?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels"> Photo by eberhard grossgasteiger from Pexels </a>`
     })
 
+// Crypto
+
 const cryptoIcon = document.getElementById("crypto-icon") //
 const cryptoInfo = document.getElementById("crypto-info")
 
@@ -27,6 +31,7 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
         document.getElementById("crypto").innerHTML = "Cannot get Crypto data at this time, try again later"
     })
 
+// Current Time
 
 function updateTime() {
     const date = new Date();
@@ -35,3 +40,58 @@ function updateTime() {
 }
 
 setInterval(updateTime, 500)
+
+// Weather
+
+    const weatherIcon = document.getElementById("weather-icon")
+    const weatherTemp = document.getElementById("weather-temp")
+    const weatherLocation = document.getElementById("weather-location")
+
+navigator.geolocation.getCurrentPosition((position) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=b771978343c59359f12e0dd933a8aaa7&units=metric`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Weather data not available")
+            }  return res.json()
+        })
+        .then(data => {
+            console.log(data)
+            console.log(data.main.temp)
+            console.log(data.weather[0].description)
+            console.log(data.name)
+
+            weatherTemp.innerHTML = `${Math.round(data.main.temp)}°`
+            weatherLocation.innerHTML = data.name
+
+            
+            switch (data.weather[0].description) {
+                case "clear sky":
+                    weatherIcon.innerHTML = `<i class="fa-solid fa-sun"></i>`
+                    break;
+                case "broken clouds" || "few clouds" || "scattered clouds" :
+                    weatherIcon.innerHTML = `<i class="fa-solid fa-cloud"></i>`
+                    break;
+                case "shower rain":
+                    weatherIcon.innerHTML = `<i class="fa-solid fa-cloud-rain"></i>`
+                    break;
+                case "rain":
+                    weatherIcon.innerHTML = `<i class="fa-solid fa-cloud-showers-heavy"></i>`
+                    break;
+                case "thunderstorm":
+                    weatherIcon.innerHTML = `<i class="fa-solid fa-cloud-bolt"></i>`
+                    break;
+                case "snow":
+                    weatherIcon.innerHTML = `<i class="fa-solid fa-snowflake"></i>`
+                    break;
+                case "mist":
+                    weatherIcon.innerHTML = `<i class="fa-solid fa-smog"></i>`
+                default:
+                    weatherIcon.innerHTML = `<i class="fa-solid fa-sun"></i>`    
+            }
+        })
+        .catch(err => console.error(err))
+    })
+
+
+
+
--- readME.md
+++ readME.md
@@ -1,2 +1,7 @@
 https://css-tricks.com/perfect-full-page-background-image/#awesome-easy-progressive-css3-way - bg image 
 Photo by eberhard grossgasteiger: https://www.pexels.com/photo/down-angle-photography-of-red-clouds-and-blue-sky-844297/ - alt bg
+
+
+openweathermap API
+Unsplash API
+geolocation
